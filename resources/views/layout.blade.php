15e02a1e25705162ee98e174d723cf2da67e331e
galasky05 <143463688+galasky05@users.noreply.github.com>
2025-05-08 23:17:00 +0700

    Part 3

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -91,9 +91,9 @@
       </div>
 
       <div class="col-md-9">
-        <div class="content">
+        
           @yield('content')
-        </div>
+        
       </div>
     </div>
   </div> <!-- penutup .container -->
