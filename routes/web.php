f867b805b878c705d34c1bfb0d15aa99d9d16ab1
galasky05 <143463688+galasky05@users.noreply.github.com>
2025-05-10 01:49:20 +0700

    Part 6

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\CourseController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +24,4 @@ Route::get('/', function () {
 
 Route::resource('/students', StudentController::class);
 Route::resource('/teachers', TeacherController::class);
+Route::resource('/courses', CourseController::class);
