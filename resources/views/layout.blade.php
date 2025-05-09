d76eab10c1c80c34098d7f687909e1683ab7f9e6
galasky05 <143463688+galasky05@users.noreply.github.com>
2025-05-10 00:12:54 +0700

    Part 4

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -82,8 +82,8 @@
       <div class="col-md-3">
         <div class="sidebar">
           <a class="active" href="#home">Home</a>
-          <a href="#news">Student</a>
-          <a href="#contact">Teacher</a>
+          <a href="{{ url('/students') }}">Student</a>
+          <a href="{{ url('/teachers') }}">Teacher</a>
           <a href="#contact">Courses</a>
           <a href="#about">Enrollment</a>
           <a href="#about">Payment</a>
